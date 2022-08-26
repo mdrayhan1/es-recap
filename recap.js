b66e03a208fdc15f302177e264cc7f7733521b11
mdrayhan6 <mdrayhanm2525@gmail.com>
2022-08-26 21:17:10 +0600

    change

---
+++ recap.js
@@ -0,0 +1,37 @@
+/* 
+1. var,let, const
+2. template string
+  2.1: crete multi-line string (``)
+  2.2: add dynamic variable (expression): `${}`
+3. Arrow function
+  3.1: single parameter arrow function
+  3.2: two parameters arrow function
+  3.3: arrow function with function body.
+   And dont forget you have to return explicitly
+4. map, filter, find
+5. optional chaining
+
+  */
+
+
+  //GOOD TO HAVE
+ /*
+ 1.spread operatorv: [...numbers, 12, 45]
+ 2.destructing
+  2-1: object destructuring. how to get property name eas variable
+       const {x} = {x: 3, y:5}
+  2-2: array destructuring
+       const [a, b] = [3, 8, 1]
+       
+ 3.default parameter
+ 4.forEach
+ 5.class, object
+ 6. dot notation vs bracket notation
+ */
+
+ // should Know THE name
+ /* 
+ 1. reduce
+ 2.inheritance
+ 3.prototypical chain
+ */
